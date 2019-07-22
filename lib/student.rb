9a62054570f391dfe2191bb55defcfabefda8263
janetmndz <janetmendez.03@gmail.com>
2019-07-21 23:55:28 -0400

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,14 @@
+require_relative './concerns/memorable.rb'
+
+class Student
+    include Greeting
+    # def hello
+    #     puts "Hey there! I'm so excited to learn stuff."
+    # end
+
+    # def raise_hand
+    #     super
+    #     super
+    #     super
+    # end
+end
